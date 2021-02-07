04e09a29f1e228508c4c11b2283e06d38aae3c4d
ejsmith13 <ejsmithwork@gmail.com>
2021-02-07 13:21:36 -0500

    added switch statements for engineer and intern, returns data

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 
-
-
 const questions = [
   {
     name: "name",
@@ -27,29 +25,27 @@ const questions = [
     name: "role",
     message: "What is the employee's role?",
     type: "list",
-    choices: ["manager", "engineer", "intern"]
+    choices: ["manager", "engineer", "intern"],
   },
   {
     name: "office",
     message: "What is their office number?",
     type: "input",
-    when: (answer) => answer.role === "manager"
+    when: (answer) => answer.role === "manager",
   },
   {
     name: "github",
     message: "What is the employee's GitHub user name?",
     type: "input",
-    when: (answer) => answer.role === "engineer"
+    when: (answer) => answer.role === "engineer",
   },
   {
     name: "school",
     message: "What school do they attend?",
     type: "input",
-    when: (answer) => answer.role === "intern"
+    when: (answer) => answer.role === "intern",
   },
 ];
-
-
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -60,25 +56,40 @@ function writeToFile(fileName, data) {
 
 function init() {
   inquirer.prompt([...questions]).then((response) => {
-    const {name, id, email, role, github, office, school}= response
-    
-    switch (role){
-      case 'manager':
-        console.log("manager")
-        const tile= new Manager(name, id, email, office);
-        tile.getName()
-        tile.getId()
-        tile.getEmail()
-        tile.getRole()
-        tile.getOffice()
-        
-      default:
-        return "Please enter data"
-    }
-    
-    
+    const { name, id, email, role, github, office, school } = response;
 
-    
+    switch (role) {
+      case "manager":
+        console.log("manager");
+        const manager = new Manager(name, id, email, office);
+        manager.getName();
+        manager.getId();
+        manager.getEmail();
+        manager.getRole();
+        manager.getOffice();
+        break;
+      case "engineer":
+        console.log("eningeer");
+        const engineer = new Engineer(name, id, email, github);
+        engineer.getName();
+        engineer.getId();
+        engineer.getEmail();
+        engineer.getRole();
+        engineer.getGitHub();
+        break;
+      case "intern":
+        console.log("intern");
+        const intern = new Intern(name, id, email, school);
+        intern.getName();
+        intern.getId();
+        intern.getEmail();
+        intern.getRole();
+        intern.getSchool();
+        break;
+
+      default:
+        return "Please enter data";
+    }
 
     // const answer = generateHTML.createPage(response);
 
